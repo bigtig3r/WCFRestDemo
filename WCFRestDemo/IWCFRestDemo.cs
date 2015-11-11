119cbebd5a2c1a72901bbbe54851513cb052bf6d
John Dat Tran (Mr.) <trandat.nguyentd@gmail.com>
2015-11-12 01:05:42 +0700

    check in

---
+++ WCFRestDemo/IWCFRestDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFRestDemo
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IWCFRestDemo" in both code and config file together.
+    [ServiceContract]
+    public interface IWCFRestDemo
+    {
+        [OperationContract]
+        [WebGet(RequestFormat=WebMessageFormat.Json,
+            ResponseFormat=WebMessageFormat.Json)]
+        string getMessage();
+        [OperationContract]
+        [WebInvoke(Method="POST",
+            RequestFormat=WebMessageFormat.Json,
+            ResponseFormat=WebMessageFormat.Json)]
+        string postMessage(string userName);
+    }
+}
---
+++ WCFRestDemo/WCFRestDemo.svc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFRestDemo
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WCFRestDemo" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select WCFRestDemo.svc or WCFRestDemo.svc.cs at the Solution Explorer and start debugging.
+    public class WCFRestDemo : IWCFRestDemo
+    {
+        public string getMessage()
+        {
+            return "Hello";
+        }
+        public string postMessage(string userName)
+        {
+            return string.Format("Wellcom {0}", userName);
+        }
+    }
+}
